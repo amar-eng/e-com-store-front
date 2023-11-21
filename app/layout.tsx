79244cc05395a6e56048e167081af9673b137ec8
amar-eng <am24mo@gmail.com>
2023-11-21 14:28:55 -0500

    changes to the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import { Lato } from 'next/font/google';
+import { Baskervville } from 'next/font/google';
 
 import Footer from '@/components/footer';
 import Navbar from '@/components/navbar';
@@ -8,8 +8,8 @@ import './globals.css';
 import ModalProvider from '@/providers/modal-provider';
 import ToastProvider from '@/providers/toast-provider';
 
-const font = Lato({
-  weight: ['100', '300', '400', '700', '900'],
+const font = Baskervville({
+  weight: ['400'],
   subsets: ['latin'],
 });
 
